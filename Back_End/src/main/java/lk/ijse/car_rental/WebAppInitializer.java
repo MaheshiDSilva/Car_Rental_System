0514e6b4a0e38196f4d52daddf4b2fb5144608ef
MaheshiDSilva <maheshi99silva@gmail.com>
2023-10-25 19:14:45 +0530

    Content Updated

---
+++ Back_End/src/main/java/lk/ijse/car_rental/WebAppInitializer.java
@@ -0,0 +1,5 @@
+package lk.ijse.car_rental;
+
+public class WebAppInitializer {
+
+}
---
+++ Back_End/src/main/java/lk/ijse/car_rental/util/ResponseUtil.java
@@ -0,0 +1,17 @@
+package lk.ijse.car_rental.util;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
+@ToString
+public class ResponseUtil {
+    private String state;
+    private String message;
+    private Object data;
+
+}
