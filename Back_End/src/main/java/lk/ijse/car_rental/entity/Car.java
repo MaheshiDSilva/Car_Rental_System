8322b844af2ef66ff88f43d110677366f56b66a6
MaheshiDSilva <maheshi99silva@gmail.com>
2023-10-27 16:16:07 +0530

    Content Updated

--- Back_End/src/main/java/lk/ijse/car_rental/entity/Car.java
+++ Back_End/src/main/java/lk/ijse/car_rental/entity/Car.java
@@ -1,4 +1,43 @@
 package lk.ijse.car_rental.entity;
 
+import lk.ijse.car_rental.embedded.CarPhoto;
+import lk.ijse.car_rental.embedded.FreeMileage;
+import lk.ijse.car_rental.embedded.PriceRate;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
+
+import javax.persistence.Embedded;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import java.math.BigDecimal;
+
+@Entity
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
+@ToString
 public class Car {
+    @Id
+    private String regNum;
+    private String color;
+    private String type;
+    private String brand;
+    private String availability;
+    private String transmissionType;
+    private String fuelType;
+    private int passengers;
+    private BigDecimal extraKMPrice;
+    private BigDecimal lostDamageCost;
+    private String meterValue;
+    @Embedded
+    private PriceRate priceRate;
+    @Embedded
+    private FreeMileage freeMileage;
+    @Embedded
+    private CarPhoto carPhoto;
+
+
+
 }
---
+++ Back_End/src/main/java/lk/ijse/car_rental/entity/Customer.java
@@ -0,0 +1,31 @@
+package lk.ijse.car_rental.entity;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
+
+import javax.persistence.CascadeType;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.OneToOne;
+
+@Entity
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
+@ToString
+public class Customer {
+    @Id
+    private String nic;
+    private String name;
+    private String address;
+    private String contact;
+    private String email;
+    private String license;
+    private String nicImage;
+    private String licenseImage;
+    @OneToOne(cascade = CascadeType.ALL)
+    private User user;
+
+}
