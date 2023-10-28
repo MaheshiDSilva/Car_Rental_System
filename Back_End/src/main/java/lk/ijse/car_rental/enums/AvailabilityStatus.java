10a6869810c77c65498a58884717e9bc59d48005
MaheshiDSilva <maheshi99silva@gmail.com>
2023-10-28 15:29:32 +0530

    Content Updated

---
+++ Back_End/src/main/java/lk/ijse/car_rental/enums/AvailabilityStatus.java
@@ -0,0 +1,5 @@
+package lk.ijse.car_rental.enums;
+
+public enum AvailabilityStatus {
+    YES, NO, MAINTAIN
+}
---
+++ Back_End/src/main/java/lk/ijse/car_rental/repo/CarRepo.java
@@ -0,0 +1,30 @@
+package lk.ijse.car_rental.repo;
+
+import lk.ijse.car_rental.entity.Car;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+
+import java.util.List;
+
+public interface CarRepo extends JpaRepository<Car,String> {
+
+    Car findCarByRegNum(String regNum) throws RuntimeException;
+
+    @Query(value = "select count(regNum) from Car where Availability='YES'", nativeQuery = true)
+    int countReservedCars()throws RuntimeException;
+
+    @Query(value = "select count(regNum) from Car where Availability='NO'", nativeQuery = true)
+    int countAvailableCars() throws RuntimeException;
+
+    List<Car> findByRegNumAndFuelType(String regNum,String fuelType) throws RuntimeException;
+
+    List<Car> findByBrandAndFuelType(String brand,String fuelType) throws RuntimeException;
+
+    List<Car> findByColorAndFuelType(String color,String fuelType) throws RuntimeException;
+
+    @Query(value = "select brand, count(brand) from Car group by brand",nativeQuery = true)
+    List countCarBrands() throws RuntimeException;
+
+    @Query(value = "select count(regNum) from Car where Availability='MAINTAIN'", nativeQuery = true)
+    List countMaintainingCars() throws RuntimeException;
+}
