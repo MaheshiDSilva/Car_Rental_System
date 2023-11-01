da93b0195c1080462617c81cf86625801064879c
MaheshiDSilva <maheshi99silva@gmail.com>
2023-11-02 02:51:14 +0530

    Content Updated

--- Front_End/controller/AdminPageController.js
+++ Front_End/controller/AdminPageController.js
@@ -13,6 +13,7 @@ $.ajax({
 });
 
 loadHome();
+manageHomePage();
 
 function loadHome() {
     $('#home').fadeIn();
@@ -26,4 +27,155 @@ function loadHome() {
     $("#rents").attr("style", "display : none !important");
     $("#payments").attr("style", "display : none !important");
     $("#reports").attr("style", "display : none !important");
+}
+
+
+function manageHomePage() {
+
+    $("#btnHome").on("click", function () {
+        loadHome();
+        loadData();
+
+    });
+
+    loadData();
+
+    function loadData() {
+        $.ajax({
+            url: baseUrl + "customer/count",
+            method: "get",
+            dataType: "json",
+            success: function (res) {
+                $("#reg-users").text(res.data);
+            }
+        });
+
+        $.ajax({
+            url: baseUrl + "rent/count",
+            method: "get",
+            dataType: "json",
+            success: function (res) {
+                $("#rent-count").text(res.data);
+            }
+        });
+
+        $.ajax({
+            url: baseUrl + "car/count",
+            method: "get",
+            dataType: "json",
+            success: function (res) {
+                $("#available-cars").text(res.data);
+            }
+        });
+
+        $.ajax({
+            url: baseUrl + "car/count/reserved",
+            method: "get",
+            dataType: "json",
+            success: function (res) {
+                $("#reserved-cars").text(res.data);
+            }
+        });
+
+        $.ajax({
+            url: baseUrl + "car/count/maintain",
+            method: "get",
+            dataType: "json",
+            success: function (res) {
+                $("#maintain-cars").text(res.data);
+            }
+        });
+
+        $.ajax({
+            url: baseUrl + "driver/available",
+            method: "get",
+            dataType: "json",
+            success: function (res) {
+                $("#available-drivers").text(res.data);
+            }
+        });
+
+        $.ajax({
+            url: baseUrl + "driver/reserved",
+            method: "get",
+            dataType: "json",
+            success: function (res) {
+                $("#reserved-drivers").text(res.data);
+            }
+        });
+
+    }
+
+    var dataPoints = [];
+
+    var options = {
+        animationEnabled: true,
+        theme: "light2",
+        title: {
+            text: "Daily Sales Income"
+        },
+        axisX: {
+            valueFormatString: "DD MMM YYYY",
+        },
+        axisY: {
+            title: "LKR",
+            titleFontSize: 24
+        },
+        data: [{
+            type: "spline",
+            yValueFormatString: "$#,###.##",
+            dataPoints: dataPoints
+        }]
+    };
+
+    $.ajax({
+        url: baseUrl + "payment/daily",
+        method: "get",
+        success: function (res) {
+            for (var i = 0; i < res.data.length; i++) {
+                dataPoints.push({
+                    x: new Date(res.data[i][0]),
+                    y: res.data[i][1]
+                });
+            }
+            $("#chartContainer").CanvasJSChart(options);
+        }
+    });
+
+    let points = [];
+
+    var brandOptions = {
+        title: {
+            text: "Car Brands"
+        },
+        subtitles: [{
+            text: "About Car Brands"
+        }],
+        animationEnabled: true,
+        data: [{
+            type: "pie",
+            startAngle: 40,
+            toolTipContent: "<b>{label}</b>: {y}%",
+            showInLegend: "true",
+            legendText: "{label}",
+            indexLabelFontSize: 16,
+            indexLabel: "{label} - {y}%",
+            dataPoints: points
+        }]
+    };
+
+    $.ajax({
+        url: baseUrl + "car/brand",
+        method: "get",
+        success: function (res) {
+            for (var i = 0; i < res.data.length; i++) {
+                points.push({
+                    y: res.data[i][1],
+                    label: res.data[i][0]
+                });
+            }
+            $("#brandChart").CanvasJSChart(brandOptions);
+        }
+    });
+
 }
